eb52795f254cbb7b13b751f1a95757968249c85f
sumit4689 <sumitgatpatil41@gmail.com>
2025-01-29 11:25:03 +0530

    data change

--- src/components/Events/ContainerHover.jsx
+++ src/components/Events/ContainerHover.jsx
@@ -12,22 +12,22 @@ const ContainerHover = () => {
         backgroundImage: 'https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExcnJrN3MxdTk5ZGppczJyNXhmY3JkZzM3OWFtNGIyNzl6czc3Z3BvcyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/bGgsc5mWoryfgKBx1u/giphy.gif'
       },
       { 
-        title: 'Non-Technical', 
+        title: 'Non-Technical Events', 
         description: 'Discover pristine shorelines and crystal clear waters.',
         backgroundImage: 'https://picsum.photos/240/320?random=2'
       },
       { 
-        title: 'Cultural', 
+        title: 'Cultural Events', 
         description: 'Experience tranquil waters reflecting the surrounding landscape.',
         backgroundImage: 'https://picsum.photos/240/320?random=4'
       },
       { 
-        title: 'Sports', 
+        title: 'Sports Events', 
         description: 'Witness the beauty of ancient and towering forests.',
         backgroundImage: 'https://i.pinimg.com/originals/22/dc/02/22dc02c55c76190ad9deed1e5c81426b.gif'
       },
       { 
-        title: 'E-Sports', 
+        title: 'E-Sports Events', 
         description: 'Witness the beauty of ancient and towering forests.',
         backgroundImage: 'https://i.gifer.com/3PtI.gif'
       },
