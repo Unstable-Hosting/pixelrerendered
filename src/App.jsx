aef68d6469bf1d51b76fd7db7fac9e94cda314e7
Dinesh2112 <dineshpandian1931@gmail.com>
2025-02-23 23:41:21 +0530

    audio page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "./components/NavBar";
+import PreEvent from "./components/Pre-event/PreEvent";
 
 function App() {
   return (
     <section className="App w-[100%] h-[100%] bg-black">
-      <NavBar />
-      <Outlet />
+      {/* <NavBar />
+      <Outlet /> */}
+      <PreEvent/>
     </section>
   );
 }
