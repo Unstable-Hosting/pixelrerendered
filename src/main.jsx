961249a4f18bb5147a22065ac538f97791777bdf
Varun <varunkrishnankv2002@gmail.com>
2025-02-23 23:53:41 +0530

    Pre-event Addedd as the index

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-// import App from "./App.jsx";
-import PreEvent from "./components/Pre-event/PreEvent";
+import App from "./App.jsx";
+// import PreEvent from "./components/Pre-event/PreEvent";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 // import LandingPage from "./pages/LandingPage";
 // import EventInfo from './components/DynamicEventsPage/EventInfo'
 // import DynamicEventsPage from "./components/DynamicEventsPage/DynamicEventsPage";
@@ -16,31 +17,31 @@ const removePreloader = () => {
 };
 
 // Comment out the old router configuration
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <App />,
-//     children: [
-//       {
-//         index: true,
-//         element: <LandingPage />,
-//       },
-//       {
-//         path: "events/:title",
-//         element: <DynamicEventsPage />,
-//       },
-//       {
-//         path: "/event-info/:title/:eventName",
-//         element: <EventInfo />
-//       }
-//     ],
-//   },
-// ]);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    // children: [
+    //   {
+    //     index: true,
+    //     element: <LandingPage />,
+    //   },
+    //   {
+    //     path: "events/:title",
+    //     element: <DynamicEventsPage />,
+    //   },
+    //   {
+    //     path: "/event-info/:title/:eventName",
+    //     element: <EventInfo />
+    //   }
+    // ],
+  },
+]);
 
 window.onload = removePreloader;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <PreEvent />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
