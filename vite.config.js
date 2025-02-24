3179fa973d9e33b39c217b24896d6550e59101d6
Alone_X <62175217+AloneX079@users.noreply.github.com>
2025-02-24 22:24:39 +0530

    Update vite.config.js
    Typo.

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,
-    allowedHosts: [
-      'localhost',
-      'pixelstechfest.in'
-    ]
+    host: true
+  },
+  preview: {
+    allowedHosts: ['pixelstechfest.in']
   }
-})
+});
+
+
