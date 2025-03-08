8332829f50d71fd73ed95e3a46e8aeb4aebddee5
mca24026 <mca24026@siescoms.in>
2025-03-08 16:12:51 +0530

    Wrap Up

--- src/components/Hero/Timer.jsx
+++ src/components/Hero/Timer.jsx
@@ -1,13 +1,13 @@
 // import React from "react";
-import CountDownTimer from "./CountDownTimer.jsx";
+// import CountDownTimer from "./CountDownTimer.jsx";
 
 const Timer = () => {
   return (
     <div className="mt-8 w-full">
       <div className="date">
-        <p className="text-center">7 March 2025 - 8 March 2025</p>
+        <p className="text-center">Pixels Tech Fest 2025 has concluded!<br></br> Thank you for joining us on March 7th and 8th.<br></br> Your energy and engagement were fantastic.<br></br> We're already looking forward to the next Pixels!</p>
       </div>
-      <CountDownTimer />
+      {/* <CountDownTimer /> */}
     </div>
   );
 };
