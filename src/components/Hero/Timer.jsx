3d46fa8d3de0aed2e4e37885355eccc0bc65466d
Alone_X <62175217+AloneX079@users.noreply.github.com>
2025-03-09 16:58:01 +0530

    Closure LTS
    -Font Bold

--- src/components/Hero/Timer.jsx
+++ src/components/Hero/Timer.jsx
@@ -5,7 +5,7 @@ const Timer = () => {
   return (
     <div className="mt-8 w-full">
       <div className="date">
-        <p className="text-center font-serif">
+        <p className="text-center font-serif font-bold">
           Pixels Tech Fest 2025 has concluded!<br></br> Thank you for joining us
           on March 7th and 8th.<br></br> Your energy and engagement were
           fantastic.<br></br> We&apos;re already looking forward to the next
